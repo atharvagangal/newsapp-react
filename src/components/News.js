6555e0f0657fe71288618d79f2f485ce26aff302
Atharva Gangal <gangal.atharva@gmail.com>
2021-10-12 22:59:26 +0530

    Added Navbar, News & NewsItem components.

---
+++ src/components/News.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+
+export class News extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      page: 1,
+      loading: false,
+    };
+  }
+
+  async componentDidMount() {
+    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=aa5b80f5192f427bbc72337c48bf4f3f&page=1&pageSize=20";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults });
+  }
+
+  handlePrevClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=aa5b80f5192f427bbc72337c48bf4f3f&page=${this.state.page - 1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    });
+  };
+  handleNextClick = async () => {
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=aa5b80f5192f427bbc72337c48bf4f3f&page=${this.state.page + 1}&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h1 className="my-3">NewsMuse - Top Headlines</h1>
+        <div className="row">
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4 my-3" key={element.url}>
+                <NewsItem title={element.title} description={element.description} imgUrl={element.urlToImage} newsUrl={element.url} />
+              </div>
+            );
+          })}
+        </div>
+        <div className="container d-flex justify-content-between my-3">
+          <button disabled={this.state.page <= 1} type="button" className="btn btn-dark mx-3" onClick={this.handlePrevClick}>
+            &larr; Previous
+          </button>
+          <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>
+            Next &rarr;
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default News;
